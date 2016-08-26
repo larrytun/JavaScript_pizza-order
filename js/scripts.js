9ed79b97cff63a3106d42e6e48471aa220e06a01
Larry Tunseth <larrydeantun@gmail.com>
2016-08-26 13:00:42 -0700

    add objects prototypes and constructors to Javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
+// BS
+function Pizza (name, size) {
+  this.customerName = name;
+  this.pizzaSize = size;
+  this.toppings = [];
+  this.totalCost = 9;
+}
+function Topping (toppingType) {
+  this.topping = toppingType;
+}
+
+Pizza.prototype.finalPizza = function() {
+  return this.customerName + ", " + this.pizzaSize + ", " + this.toppings + ", " + this.totalCost;
+}
+
+
+
+
+
+
+
+// UI
 $(document).ready(function() {
   $("#addMoreToppings").click(function() {
     $("#newToppings").append('<div class ="newTopping">' +
@@ -10,6 +32,25 @@ $(document).ready(function() {
                                 '<option>Onions</option>' +
                                 '<option>Bacon</option>' +
                               '</select>' +
-                            '</div>')
+                            '</div>');
   });
+
+  $("form#newPizzaOrder").submit(function(event) {
+    event.preventDefault();
+    debugger;
+
+    var inputtedCustomerName = $("input#new-customer-name").val();
+    var inputtedPizzaSize = $("#newPizzaSize :selected").val();
+    var newPizza = new Pizza(inputtedCustomerName, inputtedPizzaSize);
+
+    $(".newTopping").each(function() {
+      var inputtedTopping = $(this).find("#newPizzaToppings :selected").val();
+      var newSingleTopping = new Topping(inputtedTopping)
+      newPizza.toppings.push(newSingleTopping)
+    });
+
+    $("ul#pizzaPlace").append("<li><span class='pizza'>" + newPizza.finalPizza() +  "</span></li>");
+
+  });
+
 });
