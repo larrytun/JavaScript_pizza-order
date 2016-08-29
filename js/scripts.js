79176942e2b2d0cac07b5c213cf82142aace28ad
larrytun <larrydeantun@gmail.com>
2016-08-28 19:59:24 -0700

    fix indentation and remove spaces

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//Backend
 function Pizza(name, size) {
   this.customerName = name;
   this.pizzaSize = size;
@@ -5,21 +6,20 @@ function Pizza(name, size) {
   this.totalCost = 9;
 }
 
-
-Pizza.prototype.sizePrice = function(){
-  if (this.pizzaSize === "Large"){
+Pizza.prototype.sizePrice = function() {
+  if (this.pizzaSize === "Large") {
     this.totalCost += 6;
   } else if (this.pizzaSize === "Medium") {
     this.totalCost += 2;
   }
   if (this.toppings.length > 1) {
-    this.totalCost += ((this.toppings.length * 1.5)-1.5);
+    this.totalCost += ((this.toppings.length * 1.5) - 1.5);
     return this.totalCost;
   } else {
     return this.totalCost;
   }
-  
 };
+
 Pizza.prototype.finalName = function() {
   return this.customerName;
 };
@@ -34,12 +34,8 @@ Pizza.prototype.finalCost = function() {
 };
 
 
-
-
-
-// UI
+// User Interace
 $(document).ready(function() {
-
   $("#addMoreToppings").click(function() {
     $("#newToppings").append('<div class ="newTopping">' +
       '<select class="form-control" id="newPizzaToppings">' +
@@ -53,32 +49,23 @@ $(document).ready(function() {
       '</select>' +
       '</div>');
   });
-
   $("form#newPizzaOrder").submit(function(event) {
     event.preventDefault();
     $(".pizzaSize").empty();
     $(".pizzaToppings").empty();
     $(".customers-name").empty();
     $(".finalPizzaCost").empty();
-
     var inputtedCustomerName = $("input#new-customer-name").val();
     var inputtedPizzaSize = $("#newPizzaSize :selected").val();
     var newPizza = new Pizza(inputtedCustomerName, inputtedPizzaSize);
-
-  
-
     $(".newTopping").each(function() {
       var inputtedTopping = $(this).find("#newPizzaToppings :selected").val();
       var newSingleTopping = inputtedTopping;
       newPizza.toppings.push(newSingleTopping);
-      
     });
-
     $(".customers-name").append(newPizza.finalName());
     $(".pizzaSize").append(newPizza.finalSize());
     $(".pizzaToppings").append(newPizza.finalTopping() + "  ");
     $(".finalPizzaCost").append("$" + newPizza.sizePrice());
-
   });
-
 });
